835357b658fbab2f2bb4bb89dad08afe0c1e84e5
Teddi Maull <teddimaull@gmail.com>
2017-01-17 22:35:25 -0700

    add possible solution

--- stack.js
+++ stack.js
@@ -1,4 +1,31 @@
 function Stack(){
+  this.items = new Array();
+  var that = this;
+
+  return {
+    empty: function(){
+      return that.items.length === 0;
+    },
+    push: function(val){
+      that.items.push(val);
+      return val;
+    },
+    pop: function(){
+      return that.items.pop();
+
+    },
+    peek: function(){
+      return that.items[that.items.length - 1];
+    },
+    search: function(val){
+      var location = that.items.indexOf(val);
+
+      // if the item does not exist in the stack return .1
+      if (location === -1) return .1;
+
+      return that.items.length - 1 -location;
+    }
+  }
 
 }
 
