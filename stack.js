b6fd754444f2310b622c3b337a9d2b6fe1fc4635
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-18 10:20:47 -0700

    :tada: all tests passing

--- stack.js
+++ stack.js
@@ -1,5 +1,45 @@
-function Stack(){
+'use strict';
 
+function Stack() {
+  const myStack = [];
+
+  return {
+    empty,
+    peek,
+    push,
+    pop,
+    search,
+  };
+
+  function empty() {
+    if (myStack.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function peek() {
+    return myStack[(myStack.length - 1)];
+  }
+
+  function push(item) {
+    myStack[myStack.length] = item;
+    return item;
+  }
+
+  function pop() {
+    return myStack.splice((myStack.length - 1), 1);
+  }
+
+  function search(item) {
+    for (let i = (myStack.length - 1); i >= 0; i--) {
+      if (myStack[i] === item) {
+        return i;
+      }
+    }
+    return 0.1;
+  }
 }
 
 module.exports = Stack;
